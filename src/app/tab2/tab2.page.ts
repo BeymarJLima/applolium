ec54f32ec88fc77c3ccb48b458183b2a64306100
beymar j <beymarjlima@gmail.com>
2020-03-11 07:04:24 -0300

    mudança na tela de cadastro

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit  {
+  nome: any;
+  nombre: any;
+  email: any;
+  mail: any;
+  senha: any;
+  Confsenha: any;
+  endereco: any;
+  enderec: any;  
 
   constructor() {}
+
+  ngOnInit() {
+
+  }
+
+  cadastrar() {
+    this.nome = this.nombre;
+    this.email = this.mail;
+    this.endereco = this.enderec;
+  }
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
+ 
   constructor() {}
 
 }
